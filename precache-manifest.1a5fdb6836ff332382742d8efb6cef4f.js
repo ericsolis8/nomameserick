3ac16d64a29ef14a1a7026bb51b09e402d87b5da
ericsolis8 <isc.eric97@gmail.com>
2020-09-14 20:07:34 -0500

    Updates

---
+++ precache-manifest.1a5fdb6836ff332382742d8efb6cef4f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f615ada59b72d0a80aaa8800f315bf78",
+    "url": "/nomameserick/index.html"
+  },
+  {
+    "revision": "bfbb7ed4ad92e844b767",
+    "url": "/nomameserick/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ac0f0bcf17447404accc",
+    "url": "/nomameserick/static/js/2.471f3d16.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/nomameserick/static/js/2.471f3d16.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bfbb7ed4ad92e844b767",
+    "url": "/nomameserick/static/js/main.213b8890.chunk.js"
+  },
+  {
+    "revision": "e3a5929ce14432e1033b",
+    "url": "/nomameserick/static/js/runtime-main.0c3a10a8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/nomameserick/static/media/logo.5d5d9eef.svg"
+  }
+]);
